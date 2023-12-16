76969742ce65d902f012bb1ec6fbc6934b74cb39
rayhan <aburayhandh@gmail.com>
2023-12-16 19:28:18 +0600

    How seo help section added

--- src/Components/Home/HowDoesSEOHelp.jsx
+++ src/Components/Home/HowDoesSEOHelp.jsx
@@ -1,8 +1,26 @@
 const HowDoesSEOHelp = () => {
     return (
-        <div>
-            HowDoesSEOHelp
-        </div>
+        <section className="bg-[#f7f7f7] py-24">
+            <div className="max-w-7xl mx-auto px-6 ">
+                <div className="flex flex-col xl:flex-row items-center xl:items-start gap-8">
+                    <div className="flex-1">
+                        <h2 className="text-4xl font-bold text-[#3b3663] mb-6">
+                            How Does SEO Help Your Business Growth?
+                        </h2>
+
+                        <p className="text-xl font-medium text-[#696687]">
+                            No matter the size of your business, whether you are an established large firm or just starting out, reaching more people should be your primary goal. From local to global, in today’s business world, online digital marketing is often the best way to grow your exposure to the public. Search engine optimization, or SEO, is the most reliable way of increasing your reach online. Through SEO for your website and overall online presence, you can make stronger bonds with existing customers, reach new ones, and generate sales at higher scales.
+                            <br /><br />
+                            Ultimately, search engine optimization plays the most vital role in online marketing, and Giant Marketers specializes in it. Every day, your potential customers are searching for what you sell online. Search engine optimizers like us can help lead them right to you! Partnering with an SEO specialist agency with more than a decade of experience, like us at Giant Marketers, will directly impact your ROI.
+                        </p>
+                    </div>
+
+                    <div className="flex-1">
+                        <img src="https://giantmarketers.com/wp-content/uploads/2023/08/giant-marketers-digital-marketing-agency.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
