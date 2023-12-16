0d591a525503672685b51a538a6d05bd5df5697d
rayhan <aburayhandh@gmail.com>
2023-12-16 08:07:22 +0600

    Small seo services section added

--- src/Components/Home/SmallSeoServices.jsx
+++ src/Components/Home/SmallSeoServices.jsx
@@ -1,8 +1,26 @@
 const SmallSeoServices = () => {
     return (
-        <div>
-            SmallSeoServices
-        </div>
+        <section className="max-w-7xl mx-auto px-6 py-24">
+            <div className="flex flex-col xl:flex-row items-center xl:items-start gap-8 xl:gap-2">
+                <div className="flex-1">
+                    <img src="https://giantmarketers.com/wp-content/uploads/2023/08/Affordable-SEO-Services-For-Small-Businesses-Like-You-giant-marketers.png" alt="" />
+                </div>
+
+                <div className="flex-1">
+                    <h2 className="text-4xl font-bold text-[#3b3663] mb-6">
+                        Affordable SEO Services For Small Businesses Like You!
+                    </h2>
+
+                    <p className="text-xl font-medium text-[#696687]">
+                        Do you own an online business and need organic SEO services to boost traffic to your website? Giant Marketers offers affordable SEO services for small business websites to help generate new leads and grow sales. <br /><br />
+
+                        In today’s online marketing environment, for small and medium type businesses, you will need a specialized SEO service from a team you can rely on. This is just what we have been providing to our clients for years! We will utilize all our knowledge and resources to assist you in improving your search engine ranking by optimizing your content to reach more people. <br /><br />
+
+                        As an SEO service provider company in Bangladesh, we will make your search engine performance soar without breaking the bank. Right now is the perfect time to invest in SEO!
+                    </p>
+                </div>
+            </div>
+        </section>
     );
 };
 
